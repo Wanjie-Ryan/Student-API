baeaad11451f350dcef6b8d9c8fedb08f8989854
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-02 06:11:10 +0300

    service and controller classes

--- src/main/java/com/students/tutorial/student/StudentRepository.java
+++ src/main/java/com/students/tutorial/student/StudentRepository.java
@@ -1,4 +1,10 @@
 package com.students.tutorial.student;
 
-public interface StudentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// over here you specify the type of object you want your interface to work with and the Data type for the ID, in our case the id is of data type Long
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+
+
 }
