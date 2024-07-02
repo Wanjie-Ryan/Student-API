60e0de226b0e29395b56cc06504e9087e4ed92cc
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-02 07:23:32 +0300

    post request for the student

--- src/main/java/com/students/tutorial/student/StudentController.java
+++ src/main/java/com/students/tutorial/student/StudentController.java
@@ -3,6 +3,7 @@ package com.students.tutorial.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,11 @@ public class StudentController {
 
     public List<Student> getStudents(){
         return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(Student student){
+        studentService.addNewStudent(student);
     }
 
 
